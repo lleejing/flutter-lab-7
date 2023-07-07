c80642d3d4bec6f67a7f798d90b350c83cd7d328
lleejing <LIMLEEJING@GMAIL.COM>
2023-07-08 01:40:51 +0800

    Add files via upload

---
+++ lib/gmap.dart
@@ -0,0 +1,149 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GMap extends StatefulWidget {
+  GMap({Key? key}) : super(key: key);
+
+  @override
+  _GMapState createState() => _GMapState();
+}
+
+class _GMapState extends State<GMap> {
+  Set<Marker> _markers = HashSet<Marker>();
+  Set<Polygon> _polygons = HashSet<Polygon>();
+  Set<Polyline> _polylines = HashSet<Polyline>();
+  Set<Circle> _circles = HashSet<Circle>();
+  bool _showMapStyle = false;
+
+  late GoogleMapController _mapController;
+  late BitmapDescriptor _markerIcon;
+
+  @override
+  void initState() {
+    super.initState();
+    _setMarkerIcon();
+    _setPolygons();
+    _setPolylines();
+    _setCircles();
+  }
+
+  void _setMarkerIcon() async {
+    _markerIcon =
+        await BitmapDescriptor.fromAssetImage(ImageConfiguration(), 'assets/images/noodle_icon.png');
+  }
+
+  void _toggleMapStyle() async {
+    String style = await DefaultAssetBundle.of(context).loadString('assets/images/map_style.json');
+
+    if (_showMapStyle) {
+      _mapController.setMapStyle(style);
+    } else {
+      _mapController.setMapStyle(null);
+    }
+  }
+
+  void _setPolygons() {
+    List<LatLng> polygonLatLongs = <LatLng>[];
+    polygonLatLongs.add(LatLng(37.77777, -122.45555));
+    polygonLatLongs.add(LatLng(37.78888, -122.44444));
+    polygonLatLongs.add(LatLng(37.79999, -122.43333));
+    polygonLatLongs.add(LatLng(37.74444, -122.42222));
+
+    _polygons.add(
+      Polygon(
+        polygonId: PolygonId("0"),
+        points: polygonLatLongs,
+        fillColor: Colors.white,
+        strokeWidth: 1,
+      ),
+    );
+  }
+
+  void _setPolylines() {
+    List<LatLng> polylineLatLongs = <LatLng>[];
+    polylineLatLongs.add(LatLng(37.71111, -122.44444));
+    polylineLatLongs.add(LatLng(37.72222, -122.43333));
+    polylineLatLongs.add(LatLng(37.73333, -122.42222));
+    polylineLatLongs.add(LatLng(37.74444, -122.41111));
+
+    _polylines.add(
+      Polyline(
+        polylineId: PolylineId("0"),
+        points: polylineLatLongs,
+        color: Colors.purple,
+        width: 1,
+      ),
+    );
+  }
+
+  void _setCircles() {
+    _circles.add(
+      Circle(
+          circleId: CircleId("0"),
+          center: LatLng(37.00000, -122.11111),
+          radius: 1000,
+          strokeWidth: 10,
+          fillColor: Colors.blueAccent),
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+
+    setState(() {
+      _markers.add(
+        Marker(
+            markerId: MarkerId("0"),
+            position: LatLng(37.77483, -122.41942),
+            infoWindow: InfoWindow(
+              title: "San Francsico",
+              snippet: "A city which is not Malaysia",
+            ),
+            icon: _markerIcon),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Map')),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(37.77483, -122.41942),
+              zoom: 12,
+            ),
+            markers: _markers,
+            polygons: _polygons,
+            polylines: _polylines,
+            circles: _circles,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 80),
+            child: Text("Ben's Map"),
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        child: Icon(Icons.map),
+        onPressed: () {
+          setState(() {
+            _showMapStyle = !_showMapStyle;
+          });
+
+          _toggleMapStyle();
+        },
+      ),
+    );
+  }
+}
